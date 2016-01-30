bd5e7b4ee8837169d75befa5d0b26440ea92db02
Wayne Noxchi <wayne.polatkan@gmail.com>
2016-01-30 00:46:15 -0500

    Update K3-PSET3.py

--- 6.00x/K3-PSET3.py
+++ 6.00x/K3-PSET3.py
@@ -1,6 +1,67 @@
 # 6.00.1x   -   PSet 3
 # Wayne H. Noxchi | 29-Jan-2016 14:27
 # --------------------------------------
+# 3.5: Hangman Part II: The Game/Complex Tests
+# Start: 29-Jan-2016 21:22 | Stop: 21:32 - 23:23 | Fin: 30-Jan-2016 00:35
+# ----------------------------------------------------------
+
+def hangman(secretWord):
+    '''
+    secretWord: string, the secret word to guess.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the
+      partially guessed word so far, as well as letters that the
+      user has not yet guessed.
+
+    Follows the other limitations detailed in the problem write-up.
+    '''
+    # FILL IN YOUR CODE HERE...
+
+    # guess = str
+    mistakesMade = 0
+    lettersGuessed = []
+    
+    print ("Welcome to the game, Hangman!")
+    print ("I am thinking of a word that is ") + str(wordLength(secretWord)) + (" letters long.")
+    print ("------------")
+
+    while mistakesMade < 8:
+        if isWordGuessed(secretWord, lettersGuessed):
+            return("Congratulations, you win!")
+        print ("You have %i guesses left.") % (8 - mistakesMade)
+        print ("Available letters: ") + getAvailableLetters(lettersGuessed)
+        guess = raw_input("Please guess a letter: ").lower()
+        if guess in secretWord:
+            if guess in lettersGuessed:
+                print ("Oops! You've already guessed that letter: ") + getGuessedWord(secretWord, lettersGuessed)
+                print ("------------")
+            else:
+                lettersGuessed.append(guess)
+                print ("Good guess: ") + getGuessedWord(secretWord, lettersGuessed)
+                print ("------------")
+        else:
+            if guess in lettersGuessed:
+                print ("Oops! You've already guessed that letter: ") + getGuessedWord(secretWord, lettersGuessed)
+                print ("------------")
+            else:
+                lettersGuessed.append(guess)
+                mistakesMade += 1
+                print ("Oops! That letter is not in my word: ") + getGuessedWord(secretWord, lettersGuessed)
+                print ("------------")
+    return ("Sorry, you ran out of guesses. The word was ") + secretWord + (".")
+
+def wordLength(word):
+    return len(word)
 
 # ----------------------------------------------------------
 # 3.3: Hangman Part 1: Printing Out the User's Guess
