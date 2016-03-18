428ce2fe146e475c6b3b82c70dcc63effb6faf6a
Wayne Noxchi <wayne.polatkan@gmail.com>
2016-03-17 21:20:30 -0700

    Added files via upload

---
+++ CS50x/PrC/Exercises/Ch-07.2.c
@@ -0,0 +1,29 @@
+// CS50x - Introduction to Computer Science
+// Programming in C - Exercise 7.02
+
+// Modify Program 7.1 s.t. the elements of the array values are initiually set to 0.
+//     Use a for loop to perform the initialization.
+
+#include <stdio.h>
+
+int main (void)
+{
+    int values[10];
+    int index;
+
+    for ( index = 0; index < 10; ++index )
+         values[index] = 0;
+
+    values[0] = 197;
+    values[2] = -100;
+    values[5] = 350;
+    values[3] = values[0] + values[5];
+    values[9] =
+    values[5] / 10;
+    --values[2];
+
+    for ( index = 0; index < 10; ++index )
+         printf ("values[%i] = %i\n", index, values[index]);
+
+    return 0;
+}
