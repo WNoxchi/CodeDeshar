b4167e2d30cc76b08c75f105a018332118f2c492
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-07-10 17:34:37 -0400

    Passed Pt3: Chase Robot

---
+++ CS373/Project/Pt3_ChaseAlgo_notes.py
@@ -0,0 +1,42 @@
+# 2017-Jul-10 16:45 / 2017-Jul-10 17:33
+
+# CHASE ALGORITHM V0    ########################
+#
+# HUNTING LOOP:
+#
+# REPEAT UNTIL CAUGHT:
+#     PREDICT WHERE RUNAWAY IS NOW
+#         (EKF.predict)
+#     PREDICT WHERE RUNAWAY WILL BE NEXT TIMESTEP
+#         (EKF.predict)
+#     MOVE THERE
+#         -> dst = min(max_dst, targ_dst)
+################################################
+
+# This function will be called after each time the target moves.
+def next_move(hunter_position, hunter_heading, target_measurement,
+                                                    max_distance, OTHER = None):
+
+    # initialize EKF as usual
+    if not OTHER:
+        OTHER = [EKF(1.)]
+        # near-optimal q-value (found earlier in find_good_Q.py)
+        OTHER[0].Q = matrix([[0.0035*(r==c) for r in range(OTHER[0].Q.dimx)] \
+                                        for c in range(OTHER[0].Q.dimy)])
+    EKFtr = OTHER[0]
+    # predict Runaway's current position
+    targ_est = EKFtr.predict(target_measurement)
+    # predict Runaway's next position:  ### QUESTION: does this mess up the EKF?
+    targ_est = EKFtr.predict(targ_est)
+
+    # get disance & bearing to target next-position estimate
+    targ_dst = distance_between(hunter_position, targ_est)
+    distance = min(max_distance, targ_dst)
+
+    bearing  = get_heading(hunter_position, targ_est)
+    turning  = bearing - hunter_heading
+
+    # The OTHER variable is a place for you to store any historical information about
+    # the progress of the hunt (or maybe some localization information). Your return format
+    # must be as follows in order to be graded properly.
+    return turning, distance, OTHER
